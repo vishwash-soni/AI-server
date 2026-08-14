7f5d797d12615858dfc5c2ace92bbab863511aad
Vishwash soni <sonic12345soni@gmail.com>
2026-08-14 12:21:45 +0530

    fix port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const Groq = require("groq-sdk");
 const systemPrompt = require("./systemPrompt")
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 require('dotenv').config();
 
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
